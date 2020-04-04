50d3e3c50585f19b82a92564c557ec766339ae35
Shivansh Anand <shivansh586@gmail.com>
2020-04-04 17:57:45 +0530

    cors fixed

--- finhelp/settings.py
+++ finhelp/settings.py
@@ -47,19 +47,18 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'finhelp.urls'
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ = True
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'X-AUTH',
 ]
