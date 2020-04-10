fa6780243e497080a80bc09dacd931138fdbaf0d
Shivansh Anand <shivansh586@gmail.com>
2020-04-10 17:34:37 +0530

    installed java on heroku

--- registration/views.py
+++ registration/views.py
@@ -74,26 +74,26 @@ def AddDocumentView(request):
         document.document = file
         document.save()
 
-        # try:
-        with transaction.atomic():
-            output_data = parseImportantData(document.document.path)
-            if output_data.get('form_number')!=6:
-                return HttpResponseBadRequest('This is not a valid form.')
+        try:
+            with transaction.atomic():
+                output_data = parseImportantData(document.document.path)
+                if output_data.get('form_number')!=6:
+                    return HttpResponseBadRequest('This is not a valid form.')
 
-            if output_data.get('pan_number')!=profile.pan_number:
-                return HttpResponseBadRequest('Pan Number is not matching.')
-            
-            if output_data.get('company_name')!=profile.name:
-                return HttpResponseBadRequest('This is not your ITR.')
-            output_data.pop('company_name')
-            output_data.pop('pan_number')
-            output_data.pop('form_number')
-            ITRDataset.objects.create(profile=profile,**output_data)
-            profile.status = 'Under Review'
-            profile.save()
-        # except:
-        #     document.delete()
-        #     return HttpResponseBadRequest('Please Submit a Valid Form.')
+                if output_data.get('pan_number')!=profile.pan_number:
+                    return HttpResponseBadRequest('Pan Number is not matching.')
+                
+                if output_data.get('company_name')!=profile.name:
+                    return HttpResponseBadRequest('This is not your ITR.')
+                output_data.pop('company_name')
+                output_data.pop('pan_number')
+                output_data.pop('form_number')
+                ITRDataset.objects.create(profile=profile,**output_data)
+                profile.status = 'Under Review'
+                profile.save()
+        except:
+            document.delete()
+            return HttpResponseBadRequest('Please Submit a Valid Form.')
     return JsonResponse({'status':1,"message":"file upload successful"})
 
 class DocumentListView(ListAPIView):
